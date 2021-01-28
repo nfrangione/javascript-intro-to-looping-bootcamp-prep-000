92d8b46fe09759f33632dcb06db27ec25b66ebe2
Nicholas Frangione <nfrangione8@gmail.com>
2021-01-28 21:53:15 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,11 +1,11 @@
 function forLoop(array) {
   for (let i=0; i < 25; i++) {
     i += 1;
-    if (i == 1) {
-      array = [...array, "I am 1 strange loop."];
+    if (i === 1) {
+      array.push("I am 1 strange loop.");
     }
     else {
-      array = [...array, "I am ${i} strange loops."];
+      array.push("I am ${i} strange loops.");
     }
   }
   return array;
